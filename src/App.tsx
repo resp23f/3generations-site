3024adad3ae9938da4d4ba3166df10e24a8eebbc
Alex Hoffman <ahoff@Alexs-Mac-mini.local>
2025-09-30 23:55:32 -0500

    Fix license chip markup; add wrapper to size ShieldBadge

--- src/App.tsx
+++ src/App.tsx
@@ -103,7 +103,6 @@ function HomePage() {
                 </span> 
 		<span className="truncate">{LICENSE_BADGE}</span>
               </li>
-            </ul>
     
           {/* LOGO FIRST on mobile, second on desktop */}
           <div className="order-1 lg:order-2 lg:col-span-4 lg:self-start mb-6 lg:mb-0">
