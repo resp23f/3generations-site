bdcaef6182efe2721d4763927967922a7aae07ac
Alex Hoff <ahoffman188@gmail.com>
2025-11-05 12:47:25 -0600

    Fix: add Vite env typings and enable Vercel Analytics

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 
+import { inject } from "@vercel/analytics";
+if (import.meta.env.PROD) inject();
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
